3b50aff548e071d2a7f930edbf1ab03b92f5a55f
Zeynep Sila Durak <zedurak@k1m06s05.42istanbul.com.tr>
2026-02-01 14:33:25 +0300

    Move shared utility functions to utils

---
+++ philo/src/utils.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zedurak <zedurak@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 13:46:09 by zedurak           #+#    #+#             */
+/*   Updated: 2026/02/01 13:51:22 by zedurak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_isdigit(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (!(str[i] >= '0' && str[i] <= '9'))
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+long	ft_modified_atol(char *str)
+{
+    long result;
+    int last_digit;
+
+    result = 0;
+    while (*str >= '0' && *str <= '9')
+    {
+        last_digit = *str - '0';
+        if (result > (2147483647 - last_digit) / 10)
+            write(2, "Overflow!", 9);
+        result = result * 10 + last_digit;
+		str++;
+    }
+    return (result);
+}
+
+int	str_cmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void free_and_destroy(t_all *all)
+{
+	int i;
+	int count;
+
+	count = all->number_of_philo;
+	i = 0;
+	while (count)
+	{
+		pthread_mutex_destroy(&all->forks[i]);
+		i++;
+		count--;
+	}
+	free(all->philo);
+	free(all->forks);
+}
