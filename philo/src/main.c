482e7e2f91b112669588e3827b99a951668064a2
Zeynep Sila Durak <zedurak@k1m06s03.42istanbul.com.tr>
2026-02-03 17:24:58 +0300

    Improve philosopher routine with correct timing and fork locking

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,18 +6,30 @@
 /*   By: zedurak <zedurak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 17:49:07 by zedurak           #+#    #+#             */
-/*   Updated: 2026/02/01 14:18:31 by zedurak          ###   ########.fr       */
+/*   Updated: 2026/02/03 16:48:39 by zedurak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	make_action(char *str)
+void	make_action(char *str, t_philo *philo)
 {
 	if(str_cmp(str, "eating") == 0)
-		write(1, "I'm eating..\n", 14);
+	{
+		if (philo->l_fork_id < philo->r_fork_id)
+			printf("%d and %d forks taken\n", philo->l_fork_id, philo->r_fork_id);
+		else
+			printf("%d and %d forks taken\n", philo->r_fork_id, philo->l_fork_id);
+		printf("I'm eating..\n");
+	}
 	if(str_cmp(str, "sleeping") == 0)
-		write(1, "I'm sleeping..\n", 16);
+	{
+		if (philo->l_fork_id < philo->r_fork_id)
+			printf("%d and %d forks put down\n", philo->l_fork_id, philo->r_fork_id);
+		else
+			printf("%d and %d forks put down\n", philo->r_fork_id, philo->l_fork_id);
+		printf("I'm sleeping..\n");
+	}
 	if(str_cmp(str, "thinking") == 0)
 		write(1, "I'm thinking..\n", 16);
 }
@@ -25,13 +37,45 @@ void	make_action(char *str)
 void	*start_routine(void *arg)
 {
 	t_philo *philo;
+	long eat_start;
+	long sleep_start;
 
 	philo = (t_philo*)arg;
-	pthread_mutex_lock(philo->left_fork);
-	pthread_mutex_lock(philo->right_fork);
-	make_action("eating");
-	pthread_mutex_unlock(philo->right_fork);
-	pthread_mutex_unlock(philo->left_fork);
+	while (!philo->all->someone_died)
+	{
+		if (philo->l_fork_id < philo->r_fork_id)
+		{
+			pthread_mutex_lock(philo->left_fork);
+			pthread_mutex_lock(philo->right_fork);
+			eat_start = time_in_ms();
+			make_action("eating", philo);
+			while (philo->all->someone_died == 0 && (time_in_ms() - eat_start) < philo->all->time_to_eat)
+				usleep(100);
+			pthread_mutex_unlock(philo->right_fork);
+			pthread_mutex_unlock(philo->left_fork);
+			sleep_start = time_in_ms();
+			make_action("sleeping", philo);
+			while (philo->all->someone_died == 0 && (time_in_ms() - sleep_start) < philo->all->time_to_sleep)
+				usleep(100);
+			make_action("thinking", philo);
+		}
+		else
+		{
+			pthread_mutex_lock(philo->right_fork);
+			pthread_mutex_lock(philo->left_fork);
+			eat_start = time_in_ms();
+			make_action("eating", philo);
+			while (philo->all->someone_died == 0 && (time_in_ms() - eat_start) < philo->all->time_to_eat)
+				usleep(100);
+			pthread_mutex_unlock(philo->left_fork);
+			pthread_mutex_unlock(philo->right_fork);
+			sleep_start = time_in_ms();
+			make_action("sleeping", philo);
+			while (philo->all->someone_died == 0 && (time_in_ms() - sleep_start) < philo->all->time_to_sleep)
+				usleep(100);
+			make_action("thinking", philo);
+		}
+	}
 	return (NULL);
 }
 
@@ -40,8 +84,10 @@ void	init_threads(int i, t_all *all, int number_of_philo)
 	all->philo[i].philo_id = i + 1;
 	all->philo[i].left_fork = &all->forks[i];
 	all->philo[i].right_fork = &all->forks[(i + 1) % number_of_philo];
+	all->philo[i].l_fork_id = i;
+	all->philo[i].r_fork_id = (i + 1) % number_of_philo;
 	all->philo[i].meal_count = 0;
-	all->philo[i].all_things = all;
+	all->philo[i].all = all;
 }
 
 void	start_philosophers(t_all *all, long number_of_philo)
@@ -53,11 +99,12 @@ void	start_philosophers(t_all *all, long number_of_philo)
     i = 0;
     all->philo = malloc(sizeof(t_philo)*number_of_philo);
     all->forks = malloc(sizeof(pthread_mutex_t)*number_of_philo);
+	all->start_time = time_in_ms();
     while (count > 0)
     {
-		init_threads(i, &all, number_of_philo);
+		pthread_mutex_init(&all->forks[i], NULL);
+		init_threads(i, all, number_of_philo);
         pthread_create(&all->philo[i].philos, NULL, start_routine, &all->philo[i]);
-        pthread_mutex_init(&all->forks[i], NULL);
         i++;
         count--;
     }
