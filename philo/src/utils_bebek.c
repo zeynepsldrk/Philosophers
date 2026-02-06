3208d48729a1ac2072f276f432928eab55a334d2
zeynepsldrk <162623674+zeynepsldrk@users.noreply.github.com>
2026-02-06 12:48:14 +0300

    fixed is_anyone_dead function and If must_be_eat is provided, a function that checks whether the philosophers are full has been added.

--- philo/src/utils_bebek.c
+++ philo/src/utils_bebek.c
@@ -14,7 +14,41 @@
 
 int	is_anyone_dead(t_all *all)
 {
-	if (all->someone_died)
-		return (1);
-	return (0);
+	int i;
+    long current_time;
+
+    i = 0;
+    while (i < all->number_of_philo)
+    {
+        current_time = time_in_ms();
+        if ((current_time - all->philo[i].last_meal_time) > all->time_to_die)
+        {
+            printf("%ld %d died\n", current_time - all->start_time, all->philo[i].philo_id);
+            all->someone_died = 1;
+            return (1);
+        }
+        i++;
+        if (all_eat_enough(all))
+        {
+            all->someone_died = 1;
+            return (1);
+        }
+    }
+    return (0);
+}
+
+int all_eat_enough(t_all *all)
+{
+    int i;
+
+    if (all->times_each_philo_must_eat == -1)
+        return (0);
+    i = 0;
+    while (i < all->number_of_philo)
+    {
+        if (all->philo[i].meal_count < all->times_each_philo_must_eat)
+            return (0);
+        i++;
+    }
+    return (1);
 }
