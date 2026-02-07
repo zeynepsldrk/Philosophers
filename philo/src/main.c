2a77a66958d1b0dd50368aef1cd89b0d6edd941e
Zeynep Sila Durak <zedurak@k1m17s03.42istanbul.com.tr>
2026-02-07 22:06:59 +0300

    update

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: zedurak <zedurak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 17:49:07 by zedurak           #+#    #+#             */
-/*   Updated: 2026/02/04 16:12:57 by zedurak          ###   ########.fr       */
+/*   Updated: 2026/02/07 21:48:52 by zedurak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,21 @@
 
 void	print_action(char *str, t_philo *philo, long action_time)
 {
+	pthread_mutex_lock(&philo->all->print_mutex);
     if(philo->all->someone_died)
-        return ;
+	{
+		pthread_mutex_unlock(&philo->all->print_mutex);
+		return ;
+	}
     if(str_cmp(str, "eating") == 0)
-	{
 		printf("%ld %d is eating\n", action_time - philo->all->start_time, philo->philo_id);
-        while ((time_in_ms() - action_time) < philo->all->time_to_eat)
-        {
-            if (philo->all->someone_died)
-                break;
-            usleep(500);
-        }
-	}
 	if(str_cmp(str, "sleeping") == 0)
-	{
 		printf("%ld %d is sleeping\n", action_time - philo->all->start_time, philo->philo_id);
-        while ((time_in_ms() - action_time) < philo->all->time_to_sleep)
-        {
-            if (philo->all->someone_died)
-                break;
-            usleep(500);
-        }
-	}
 	if(str_cmp(str, "thinking") == 0)
 		printf("%ld %d is thinking\n", time_in_ms() - philo->all->start_time, philo->philo_id);
+	pthread_mutex_unlock(&philo->all->print_mutex);
+	let_time_pass(philo, action_time, "eating");
+	let_time_pass(philo, action_time, "sleeping");
 }
 
 void make_action(t_philo *philo, pthread_mutex_t *first_fork, pthread_mutex_t *second_fork)
@@ -48,15 +39,18 @@ void make_action(t_philo *philo, pthread_mutex_t *first_fork, pthread_mutex_t *s
         pthread_mutex_lock(first_fork);
         if (status_check(philo, first_fork, NULL))
             break;
+		pthread_mutex_lock(&philo->all->print_mutex);
         printf("%ld %d has taken a fork\n", time_in_ms() - philo->all->start_time, philo->philo_id);
+		pthread_mutex_unlock(&philo->all->print_mutex);
         pthread_mutex_lock(second_fork);
         if (status_check(philo, first_fork, second_fork))
             break;
+		pthread_mutex_lock(&philo->all->print_mutex);
         printf("%ld %d has taken a fork\n", time_in_ms() - philo->all->start_time, philo->philo_id);
-        philo->eat_start = time_in_ms();
+		pthread_mutex_unlock(&philo->all->print_mutex);
         philo->last_meal_time = time_in_ms();
         philo->meal_count++;
-        print_action("eating", philo, philo->eat_start);
+        print_action("eating", philo, philo->last_meal_time);
         if (status_check(philo, first_fork, second_fork))
             break;
         pthread_mutex_unlock(second_fork);
@@ -78,7 +72,7 @@ void	*start_routine(void *arg)
     first_fork = NULL;
     second_fork = NULL;
 	philo = (t_philo*)arg;
-    if (philo->all->number_of_philo == 1)
+    if (philo->all->number_of_philo == 1) // allways returns true
     {
         only_one_philo(philo);
         return NULL;
@@ -86,7 +80,7 @@ void	*start_routine(void *arg)
     if (philo->all->number_of_philo % 2 == 1)
     {
         if(philo->all->number_of_philo == philo->philo_id)
-            usleep(philo->all->time_to_eat * 500);
+            usleep(philo->all->time_to_eat * 500); // already waiting for others to drop forks
     }
     make_action(philo, first_fork, second_fork);
 	return (NULL);
@@ -106,9 +100,15 @@ void	start_philosophers(t_all *all, long number_of_philo)
 	all->start_time = time_in_ms();
     while (i < number_of_philo)
     {
-		pthread_mutex_init(&all->forks[i], NULL);
 		init_threads(i, all, number_of_philo);
+		pthread_mutex_init(&all->forks[i], NULL);
         all->philo[i].last_meal_time = all->start_time;
+        i++;
+    }
+	pthread_mutex_init(&all->print_mutex, NULL);
+	i = 0;
+	while (i < number_of_philo)
+    {
         pthread_create(&all->philo[i].philos, NULL, start_routine, &all->philo[i]);
         i++;
     }
@@ -116,23 +116,23 @@ void	start_philosophers(t_all *all, long number_of_philo)
         usleep(1000);
     i = 0;
     while (i < number_of_philo)
-    {
+	{
         pthread_join(all->philo[i].philos, NULL);
-        i++;
-    }
+		i++;
+	}
 	free_and_destroy(all);
 }
 
 int	main(int argc, char **argv)
 {
-    if (argc != 5 && argc != 6)
+	int		i;
+	t_all	all;
+	if (argc != 5 && argc != 6)
 	{
 		write(2, "Wrong argument number!\n", 24);
 		return (1);
 	}
-    int i;
-    t_all all;
-
+	
     i = 1;
     while (argv[i])
     {
