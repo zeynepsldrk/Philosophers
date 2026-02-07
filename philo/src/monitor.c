8a6c2589c57835bfa75b42c563971e3ca014bdd8
Zeynep Sila Durak <zedurak@k1m17s03.42istanbul.com.tr>
2026-02-07 22:07:30 +0300

    added new functions and header

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zedurak <zedurak@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/07 13:32:26 by zedurak           #+#    #+#             */
+/*   Updated: 2026/02/07 21:45:02 by zedurak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
 int	is_anyone_dead(t_all *all)
@@ -9,10 +21,12 @@ int	is_anyone_dead(t_all *all)
     while (i < all->number_of_philo)
     {
         current_time = time_in_ms();
-        if ((current_time - all->philo[i].last_meal_time) > all->time_to_die)
+        if ((current_time - all->philo[i].last_meal_time) > all->time_to_die) // a gloabal (struct) variable for checking is a philo died
         {
+			pthread_mutex_lock(&all->print_mutex); // nobody unlocks the mutex (print)
             printf("%ld %d died\n", current_time - all->start_time, all->philo[i].philo_id);
             all->someone_died = 1;
+			pthread_mutex_unlock(&all->print_mutex);
             return (1);
         }
         i++;
@@ -52,4 +66,25 @@ int status_check(t_philo *philo, pthread_mutex_t *first_fork, pthread_mutex_t *s
         return 1;
     }
     return 0;
+}
+void let_time_pass(t_philo *philo, long action_time, char *str)
+{
+	if(str_cmp(str, "sleeping") == 0)
+	{
+		while ((time_in_ms() - action_time) < philo->all->time_to_sleep)
+		{
+			if (philo->all->someone_died)
+				break;
+			usleep(500);
+		}
+	}
+	if(str_cmp(str, "eating") == 0)
+	{
+		while ((time_in_ms() - action_time) < philo->all->time_to_eat)
+		{
+			if (philo->all->someone_died)
+				break;
+			usleep(500);
+		}
+	}
 }
