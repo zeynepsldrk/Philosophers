229988d4acae9b365c5d7d62f98b7b6a7d3d6bbe
Zeynep Sila Durak <zedurak@k1m24s03.42istanbul.com.tr>
2026-02-14 20:26:14 +0300

    fixed data race and added dead flag

--- philo/src/init.c
+++ philo/src/init.c
@@ -6,7 +6,7 @@
 /*   By: zedurak <zedurak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 14:05:41 by zedurak           #+#    #+#             */
-/*   Updated: 2026/02/13 19:48:19 by zedurak          ###   ########.fr       */
+/*   Updated: 2026/02/14 20:22:30 by zedurak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,14 @@ void    only_one_philo(t_philo *philo)
 	pthread_mutex_lock(&philo->all->print_mutex);
     printf("%ld %d has taken a fork\n", time_in_ms() - philo->all->start_time, philo->philo_id);
 	pthread_mutex_unlock(&philo->all->print_mutex);
+	pthread_mutex_lock(&philo->all->someone_died_mutex);
     while (!philo->all->someone_died)
-        usleep(1000);
+	{
+		pthread_mutex_unlock(&philo->all->someone_died_mutex);
+		usleep(1000);
+		pthread_mutex_lock(&philo->all->someone_died_mutex);
+	}
+    pthread_mutex_unlock(&philo->all->someone_died_mutex);
     pthread_mutex_unlock(philo->left_fork);
 }
 
@@ -67,6 +73,7 @@ void	init_threads_mutexs(t_all *all, long number_of_philo, int i)
     {
 		init_threads(i, all, number_of_philo);
 		pthread_mutex_init(&all->forks[i], NULL);
+		pthread_mutex_init(&all->philo[i].meal_mutex, NULL);
         all->philo[i].last_meal_time = all->start_time;
         i++;
     }
@@ -78,6 +85,9 @@ void	init_threads_mutexs(t_all *all, long number_of_philo, int i)
     }
 	while (!is_anyone_dead(all, 0))
         usleep(1000);
+	pthread_mutex_lock(&all->someone_died_mutex);
+	all->someone_died = 1;
+	pthread_mutex_unlock(&all->someone_died_mutex);
     i = 0;
     while (i < number_of_philo)
 	{
