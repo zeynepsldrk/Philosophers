95facc5eb259a6082aaa932f59253fa88c5eb54a
Zeynep Sila Durak <zedurak@k1m24s03.42istanbul.com.tr>
2026-02-14 20:28:30 +0300

    refactoring main function and start_routine function, fixed data race, clean print_action function

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: zedurak <zedurak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 17:49:07 by zedurak           #+#    #+#             */
-/*   Updated: 2026/02/13 20:46:38 by zedurak          ###   ########.fr       */
+/*   Updated: 2026/02/14 15:36:43 by zedurak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,33 @@ void	print_action(char *str, t_philo *philo, long action_time)
 		pthread_mutex_unlock(&philo->all->print_mutex);
 		return ;
 	}
-    if(str_cmp(str, "eating") == 0)
-		printf("%ld %d is eating\n", action_time - philo->all->start_time, philo->philo_id);
-	if(str_cmp(str, "sleeping") == 0)
-		printf("%ld %d is sleeping\n", action_time - philo->all->start_time, philo->philo_id);
 	if(str_cmp(str, "thinking") == 0)
 		printf("%ld %d is thinking\n", time_in_ms() - philo->all->start_time, philo->philo_id);
+	else
+		printf("%ld %d %s\n", action_time - philo->all->start_time, philo->philo_id, str);
 	pthread_mutex_unlock(&philo->all->print_mutex);
-	let_time_pass(philo, action_time);
+	if(str_cmp(str, "is eating") == 0)
+		let_time_pass(philo, philo->all->time_to_eat);
+	if(str_cmp(str, "is sleeping") == 0)
+		let_time_pass(philo, philo->all->time_to_sleep);
 }
 
 void make_action(t_philo *philo, pthread_mutex_t *first_fork, pthread_mutex_t *second_fork)
 {
-	while (!philo->all->someone_died)
+	while (1)
 	{
-		if(ft_lonely_eating(philo, first_fork, second_fork))
-			break;
-        philo->sleep_start = time_in_ms();
-        print_action("sleeping", philo, philo->sleep_start);
-        pthread_mutex_lock(&philo->all->someone_died_mutex);
+		pthread_mutex_lock(&philo->all->someone_died_mutex);
 		if (philo->all->someone_died)
 		{
 			pthread_mutex_unlock(&philo->all->someone_died_mutex);
             break;
 		}
-        print_action("thinking", philo, 0);
+		pthread_mutex_unlock(&philo->all->someone_died_mutex);
+		if(ft_lonely_eating(philo, first_fork, second_fork))
+			break;
+        philo->sleep_start = time_in_ms();
+        print_action("is sleeping", philo, philo->sleep_start);
+        print_action("is thinking", philo, 0);
 	}
 }
 
@@ -54,18 +56,17 @@ void	*start_routine(void *arg)
     pthread_mutex_t *first_fork;
     pthread_mutex_t *second_fork;
 
-    first_fork = NULL;
-    second_fork = NULL;
 	philo = (t_philo*)arg;
+	which_fork_first(philo, &first_fork, &second_fork);
     if (philo->all->number_of_philo == 1)
     {
         only_one_philo(philo);
         return NULL;
     }
-    if (philo->all->number_of_philo % 2 == 1 && !(philo->all->number_of_philo == 1))
+    if (philo->all->number_of_philo % 2 == 1)
     {
         if(philo->all->number_of_philo == philo->philo_id)
-            usleep(philo->all->time_to_eat * 500); // already waiting for others to drop forks
+            usleep(philo->all->time_to_eat * 500);
     }
     make_action(philo, first_fork, second_fork);
 	return (NULL);
@@ -85,7 +86,6 @@ void	start_philosophers(t_all *all, long number_of_philo)
 	all->start_time = time_in_ms();
 	pthread_mutex_init(&all->print_mutex, NULL);
 	pthread_mutex_init(&all->someone_died_mutex, NULL);
-	pthread_mutex_init(&all->philo->meal_mutex, NULL);
     init_threads_mutexs(all, number_of_philo, i);
 	free_and_destroy(all);
 }
@@ -96,15 +96,12 @@ int	main(int argc, char **argv)
 	t_all	all;
 	
 	if (argc != 5 && argc != 6)
-	{
-		write(2, "Wrong argument number!\n", 24);
-		return (1);
-	}
+		return (write(2, "Wrong argument number!\n", 24));
     i = 1;
     while (argv[i])
     {
         if (!ft_isdigit(argv[i]))
-            write(2, "Invalid argument!\n", 19);
+            return (write(2, "Invalid argument!\n", 19));
         i++;
     }
     all.number_of_philo = ft_modified_atol(argv[1]);
